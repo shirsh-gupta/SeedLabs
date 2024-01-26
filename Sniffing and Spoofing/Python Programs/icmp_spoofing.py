c4c4cab0f6d2fcabd85e23af6e18ab23af09953f
Shirsh Gupta <109902205+shirsh-gupta@users.noreply.github.com>
2024-01-26 18:39:54 +0530

    Delete Sniffing and Spoofing directory

--- Sniffing and Spoofing/Python Programs/icmp_spoofing.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-spoofed_source_ip = "10.9.0.6" # Replace with the IP address you want to spoof
-target_ip = "10.9.0.5" # Replace with the target IP address
-# Craft the spoofed ICMP packet
-ip_layer = IP(src=spoofed_source_ip, dst=target_ip)
-icmp_layer = ICMP()
-spoofed_packet = ip_layer / icmp_layer
-# Send the spoofed packet
-send(spoofed_packet)
